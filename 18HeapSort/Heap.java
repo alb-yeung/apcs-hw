4824b4a5dda047126fb5cdfb33c67fc9d0831fb1
Albert Yeung <alb.yeung@gmail.com>
2014-05-15 10:12:50 -0400

    19

--- 18HeapSort/Heap.java
+++ 18HeapSort/Heap.java
@@ -4,11 +4,19 @@ public class Heap{
 	heap = new int[2];
 	nextSpot = 1;
 	wall = 2;
+	min = false;
+    }
+    public Heap(int a){
+	heap = new int[2];
+	nextSpot = 1;
+	wall = 2;
+	min = true;
     }
 
     private int[] heap;
     private int nextSpot;
     private int wall;
+    private boolean min;
     private void increase(){
 	int[] temp = new int[heap.length*2];
 	for (int i = 0;i<heap.length;i++){
@@ -28,12 +36,19 @@ public class Heap{
 	}
 	heap[nextSpot]=a;
 	int placeat=nextSpot;
-	while (placeat/2>0 &&
-	       heap[placeat/2]<a
-	       ){
-	    swap(placeat,placeat/2);
-	    placeat /= 2;
-	}
+	if(!min){
+	    while (placeat/2>0 &&
+		   heap[placeat/2]<a
+		   ){
+		swap(placeat,placeat/2);
+		placeat /= 2;
+	    }
+	}else{
+	    while (placeat/2>0 &&
+		   heap[placeat/2]>a
+		   ){
+		swap(placeat,placeat/2);
+		placeat /= 2;
 	nextSpot++;
 	wall++;
     }
@@ -42,14 +57,23 @@ public class Heap{
 	if (nextSpot == 0 || wall == 1)
 	    return Integer.MIN_VALUE;
 	int max = heap[1];
-	swap(1,nextSpot-1);
+	swap(1,nextSpot - 1);
 	wall = nextSpot - 1;
 	int placeat = 1;
-	while(placeat*2 < nextSpot &&
-	      heap[placeat]<heap[placeat*2]
-	      ){
-	    swap(placeat,placeat*2);
-	    placeat *= 2;
+	if(!min){
+	    while(placeat*2 < nextSpot &&
+		  heap[placeat]<heap[placeat*2]
+		  ){
+		swap(placeat,placeat*2);
+		placeat *= 2;
+	    }
+	}else{
+	    while(placeat*2 < nextSpot &&
+		  heap[placeat]>heap[placeat*2]
+		  ){
+		swap(placeat,placeat*2);
+		placeat *= 2;
+	    }
 	}
 	nextSpot--;
 	wall --;
@@ -63,5 +87,8 @@ public class Heap{
 	}
 	return list;
     }
-
+    public int getTop(){
+	return heap[1];
+    }
+    
 }
---
+++ 19HashMapHashSet/README.txt
@@ -0,0 +1,1 @@
+A HashMap stores both keys and values with keys that point to values. In a HashSet, only keys are stored and therefore objects with same keys as previous items will override the older items.
